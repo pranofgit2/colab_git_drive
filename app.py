903e409929b032027b8128a23c02d6c19e4ea2df
pranofgit2 <120169496+pranofgit2@users.noreply.github.com>
2022-12-09 14:02:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+#import keras
+import joblib
+
+app = Flask(__name__)
+#model = keras.models.load_model('./md.h5')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+  
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    model = joblib.load("./md.pkl")
+    features =  [float(x) for x in request.form.values()]########## change##########pipeline########
+    final_features = np.reshape(np.array(features),(1,4))################# change##########pipeline#########
+    prediction =np.argmax(model.predict(final_features))
+    #prediction=1
+    
+    
+    output='Error'
+
+    if prediction==0:
+        output='Iris-setosa'
+    elif prediction==1:
+        output='Iris-versicolor'
+    else:
+        output='Iris-virginica'
+            
+
+    return render_template('index.html', prediction_text='The Iris plant spcies is  {0}{1}{2}'.format(output,final_features+100,'a'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
